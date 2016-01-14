945dfabdc08ff4872ed6cadfa9a307e8aec9ce50
Maxim Nyrko <max.nyrko@mail.ru>
2016-01-14 21:19:28 +0300

    test commit

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,34 @@
 class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
-    @answer = @question.answers.create(answer_params)
-    # redirect_to @question
-  end
+    @answer = @question.answers.new(answer_params)
 
-  private
+    respond_to do |format|
+      if @answer.save
+        format.html { redirect_to @question }
+        format.js
+        format.json do
+          response = { answers_count: @question.answers.count, answer: @answer }
+          PrivatePub.publish_to "/questions/#{@answer.question_id}/answers",
+                                response: response
+          render json: response
+        end
+      else
+        format.html { render 'questions/show' }
+        format.js
+        format.json { render json: @answer.errors.full_messages, status: :unprocessable_entity }
+      end
+end
+end
 
-  def answer_params
-    params.require(:answer).permit(:body)
-  end
+private
+
+def answer_params
+  params.require(:answer).permit(:body)
+end
+
+def check_user
+  head :forbidden unless current_user.author_of?(@answer)
+  # render nothing: true, status: :forbidden
+end
 end
